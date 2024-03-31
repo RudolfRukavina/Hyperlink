5bd02ef3ae902c647fa895f97c132a16905551df
RudolfRukavina <rukavina.rudi@gmail.com>
2024-03-31 19:58:11 +0200

    Hyperlink pripreme v0.34

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,11 +3,10 @@
     <div class="container mx-auto px-8" id='izbornik'>
       <div class="w-full flex flex-col md:flex-row py-6">
         <div class="flex-1 mb-6 text-black">
-          <a class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
-            <!--Icon from: http://www.potlabicons.com/ -->
+          <NuxtLink class="text-pink-600 no-underline hover:no-underline font-bold text-2xl lg:text-4xl" to="/">
             <img src="../assets/img/hplink.webp" alt="" class="w-32  inline-block me-3 rounded-xl">
 
-          </a>
+          </NuxtLink>
         </div>
         <div class="flex-1">
           <p class="uppercase text-gray-900 drop-shadow-md font-semibold md:mb-6">Hyperlink</p>
